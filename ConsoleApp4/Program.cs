8bc2031a76e089e2c5f0169181659d704c010162
abc <abc@DESKTOP-HM1N76V>
2024-04-21 11:12:19 +0530

    add comment

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,20 +7,18 @@ using static ConsoleApp4.Program;
 
 namespace ConsoleApp4
 {
-
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
             myclass myclass = new myclass();
             myclass.MethodToCal(OtherFunctionToCall);
             Console.ReadLine();
-        } 
+        }
         public static void OtherFunctionToCall(int i)
         {
             Console.WriteLine(i);
         }
-       
     }
 
     public class myclass
@@ -28,7 +26,8 @@ namespace ConsoleApp4
         public delegate void myDel(int i);
         public static void MethodToCal(myDel obj)
         {
-            for(int i = 0; i < 1000; i++)
+            //some long work
+            for (int i = 0; i < 1000; i++)
             {
                 obj(i);
             }
